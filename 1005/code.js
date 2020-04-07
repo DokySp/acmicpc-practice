fc7b662edb454788ddc0f3a166cae318e1275995
DokySp <uhug@naver.com>
2020-04-07 23:55:22 +0900

    1005 위상정렬 알고리즘 수정중

--- 1005/code.js
+++ 1005/code.js
@@ -15,6 +15,8 @@ var testCase = inp.splice(0,1)[0]
 
 for(var t=0; t<testCase; t++){
 
+    var start = Date.now()
+
     // N, K 입력 받기
     var nodNum = inp.splice(0,1)[0]
     var edgeNum = nodNum.split(' ')[1]
@@ -22,15 +24,15 @@ for(var t=0; t<testCase; t++){
     
     var nod = {}
     var tmp = inp.splice(0,1)[0]
+
     // 건설 시간 입력
     for(var i=0; i<nodNum; i++){
-        nod[''+i] = {}
-        nod[''+i].time = Number.parseInt(tmp.split(' ')[i])
-        nod[''+i].to = []
-        nod[''+i].from = 0
-        nod[''+i].timeTmp = []
+        nod[i] = {}
+        nod[i].time = tmp.split(' ')[i] - 0
+        nod[i].to = []
+        nod[i].from = 0
+        nod[i].timeTmp = []
     }
-    nod.length = nodNum
     
     // 연결 정보 입력
     for(var i=0; i<edgeNum; i++){
@@ -41,61 +43,57 @@ for(var t=0; t<testCase; t++){
 
     // 타겟 노드
     var targetNod = inp.splice(0,1)[0]-1
+    var endQueue = []
 
-    console.log(nod)
-    console.log()
 
     // 위상정렬
+    
+    // 인디그리 0인 노드 찾기
+    for(var i = 0; i < nodNum; i++){
+        if(nod[i].from == 0)
+            endQueue.push(i)
+    }
+    console.log(nod)
+    
     while(true){
 
+
+
+        var trig = endQueue.splice(0,1)[0]
+        // nod[trig].to
+        console.log(trig)
+
+
+
+        // ending conditions
         if(nod[targetNod].from == 0){
-            console.log(nod[targetNod].time)
+            console.log(nod[trig].time)
             break;
         }
 
-        var endNods = []
-
-        // from[].length == 0 인 노드를 찾기
-        for(var i = 0 ; i < nodNum; i++){
-            if( nod[i] != undefined && nod[i].from == 0 ){
-                
-                // 1. 부모의 time값을 자식의 timeTmp로 넘김
-                // 2. 부모에 연결된 자식의 from := false
-                //   2-1. 들어오는 노드가 여러개일 경우, 전부 지어야 하므로 들어오는 노드 값을 카운트해야 함!
-                // 3. 부모 노드를 delete (★delete 연산자 사용)
-                for(var c=0; c<nod[i].to.length; c++){
-                    nod[nod[i].to[c]].timeTmp.push(nod[i].time)
-                    endNods.push(nod[i].to[c])
-                }
-                delete nod[i]
-                nod.length--
-                
-            }
-
+	    // 자식 노드로 시간값을 보냄 & from--
+        for(var a=0; a<nod[trig].to.length; a++){
+            nod[ nod[trig].to[a] ].timeTmp.push( nod[trig].time )
+            nod[ nod[trig].to[a] ].from--
+            
+            // 자식 노드를 큐에 추가
+            endQueue.push( nod[trig].to[a] )
         }
 
-        for(c=0; c<endNods.length; c++)
-            if(nod[endNods[c]] != undefined)
-                nod[endNods[c]].from -= 1
+        // from 노드가 0일 경우, timeTmp 최댓값을 time에 추가
+        if(nod[trig].from == 0 && nod[trig].timeTmp.length != 0){
+            nod[trig].time += nod[trig].timeTmp.sort()[nod[trig].timeTmp.length-1]
+            nod[trig].timeTmp = []
+        }
         
-        // 4. timeTmp에 들어있는 값 중 최댓값을 time에 더함
-        //   4-1. from이 0이 아닌 경우, time을 더하지 않음! (동시 건설 불가능)
-        // 5. timeTmp = []
 
-        for(var i = 0 ; i < nodNum; i++){
 
-            if( nod[i] != undefined && nod[i].timeTmp.length != 0){
-                if(nod[i].from == 0){
-                    nod[i].timeTmp.sort()
-                    var max = nod[i].timeTmp[ nod[i].timeTmp.length-1 ]
-                    nod[i].time += max
-                }
-                nod[i].timeTmp = []
-            }
-        }
+        console.log(endQueue)
+        console.log(nod)
+        console.log()
+        
 
-        console.log(nod.length)
-    
     }
+
 
 }
