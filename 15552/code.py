2a0bbedd89b63d548198fed0e3d2a9ecb5e07d8c
DokySp <uhug@naver.com>
2020-04-09 18:13:39 +0900

    15552번 문제 완료

--- 15552/code.py
+++ 15552/code.py
@@ -1,7 +1,17 @@
+import sys
+# https://www.acmicpc.net/source/12521062
+# https://withcoding.com/79 (range 함수 사용법) : range(시작, 끝, 인터벌)
+# https://dodo4513.github.io/2017/07/02/python_algorithm/ (파이썬 반복문)
+# https://www.acmicpc.net/board/view/855 (sys.stdin)
 
+# sys.stdin.readline
+reader = sys.stdin
+ts = list(map(int,reader.readline().split()))
 
-ts = int(input())
+a = list(map(int,reader.read().split()))
 
-for T in range(ts):
-	
-	print(T)
+for T in range(0, ts[0]*2, 2):
+	# a = input().split(' ')
+	# res = res + str(a[0] + a[1]) + "\n"
+	print( a[T]+a[T+1] )
+
