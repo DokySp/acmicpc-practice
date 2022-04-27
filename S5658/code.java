f1a1114e39d9b6e1a3c82ead85a35735656e5953
DokySp <uhug@naver.com>
2022-04-27 17:43:58 +0900

    SWEA 5658 문제 추가

---
+++ S5658/code.java
@@ -0,0 +1,12 @@
+
+class Main{
+
+    public static void main(String args[]){
+        
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+
+        System.out.println(input);
+        
+    }
+}
